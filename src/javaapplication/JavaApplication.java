9969acbae74c80366c86547dfbb9f42967c46a0e
Jose Alonso <jalonsoriveiro@danielcastelao.org>
2019-10-16 14:26:57 +0200

    Inicio

--- src/javaapplication/JavaApplication.java
+++ src/javaapplication/JavaApplication.java
@@ -11,11 +11,11 @@ package javaapplication;
  */
 public class JavaApplication {
 
-    /**
-     * @param args the command line arguments
+    /*
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //sadasdasdasdasd
     }
     
 }
